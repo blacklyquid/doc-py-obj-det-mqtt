91b57e9af395b581b158d3c04e3dda7f05da76a0
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-07 15:32:27 -0400

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -6,13 +6,12 @@ import numpy as np
 # import configuration
 from config import Config
 from paho_mqtt import *
-from throttle import *
 from object_detector import *
 from stream_capture import *
 
 if __name__ == "__main__":
 	
-	mqtt = paho_mqtt( Config.MQTT_CLIENT_ID, Config.MQTT_USER, Config.MQTT_PASSWORD, Config.MQTT_HOST, Config.MQTT_PORT)
+	mqtt = paho_mqtt( Config.MQTT_CLIENT_ID, Config.MQTT_USER, Config.MQTT_PASSWORD, Config.MQTT_HOST, Config.MQTT_PORT, Config.THROTTLE_TIME)
 	detector = object_detector(config.MIN_CONFIDENCE, Config.FILE_PROTOTXT, Config.FILE_MODEL)
 	vs = stream_capture(Config.STREAM_URL)
 	
@@ -28,11 +27,6 @@ if __name__ == "__main__":
 
 		#Loop over the detections
 		for detection in detections:
-
-			#Filtering out weak predictions
-			#if confidence > MIN_CONFIDENCE and idx == 15:
-			if not throttle_output(detection.label, detection.timestamp, Config.THROTTLE_TIME):
-				# Publish the MQTT msg
-				mqtt.publish( Config.MQTT_TOPIC + "/" + detection.label, str(detection) )
-				print(detection, flush=True)
+			mqtt.publish( Config.MQTT_TOPIC + "/" + detection.label, str(detection) )
+			print(detection, flush=True)
 		del detections
