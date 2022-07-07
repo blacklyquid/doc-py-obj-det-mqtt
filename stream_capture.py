75b8ac65b7f3c199e560a797b76cc281a8782bbd
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-07 15:22:47 -0400

    Update stream_capture.py

--- stream_capture.py
+++ stream_capture.py
@@ -1,18 +1,14 @@
-class object_detector:
-	def __init__( self, min_confidence, prototxt, model ):
-		self.min_confidence = min_confidence
-		self.nn = cv2.dnn.readNetFromCaffe(prototxt, model)
-	def get_detections( self, blob ):
-		self.nn.setInput( blob )
-		return self._get_detected_object_list()
-	# return list of detected objects from cv2 network
-	# return only objects with confidence levels above the min
-	def _get_detected_object_list():
-		nn_detections = self.nn.forward()
-		# detected object list to return
-		dol = []
-		for i in np.arange(0, nn_detections.shape[2]):
-			if nn_detections[0, 0, i, 2] > self.min_confidence:
-				dol.append(detected_object( nn_detections[0, 0, i, 2], int(nn_detections[0, 0, i, 1])))
-
-		return dol
+class stream_capture:
+	def __init__(self, stream_url):
+		self.url = stream_url
+		self.stream = cv2.VideoCapture(self.url)
+	
+	def get_blob(self):
+		while True:
+			ret, frame = self.stream.read()
+			if ret:
+				frame = imutils.resize(frame, width=400)
+				blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
+				return blob
+	def __del__(self):
+		self.stream.release()
