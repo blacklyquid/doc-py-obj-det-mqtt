0b3e1e632c785693ee3681650873918945ae2323
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-07-07 15:31:06 -0400

    Update paho_mqtt.py

--- paho_mqtt.py
+++ paho_mqtt.py
@@ -1,10 +1,30 @@
 import paho.mqtt.client as paho
 
 class paho_mqtt:
-	def __init__( self, client_id, user, password, host, port ):
+	def __init__( self, client_id, user, password, host, port, throttle_time = 0 ):
 		self.client = paho.Client( client_id )
 		self.client.username_pw_set( user, password )
 		self.client.connect( host, port )
 		self.client.loop_start()
+		self.throttle_list = {}
+		self.throttle_time = throttle_time
+		
 	def publish( self, topic, msg ):
-		self.client.publish( topic, msg )
+		if not self.throttle_output( topic, time.time() ):
+			self.client.publish( topic, msg )
+
+	def throttle_output( topic, timestamp ):
+	    if topic in self.throttle_list:
+		# check if time since last detection
+		# if less than THROTTLE_TIME, return true, we are throttling
+		# if greater than THROTTLE_TIME, return false, we are not throttling
+		if timestamp - self.throttle_list[topic] < self.throttle_time:
+		    return True
+		else:
+		    self.throttle_list[topic] = timestamp
+		    return False
+	    else:
+		# set time in dictionary for this object
+		# return false meaning we are not throttling for this object
+		self.throttle_list[topic] = timestamp
+		return False
