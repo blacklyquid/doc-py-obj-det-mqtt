2eb5899ff86661aa64c85ce9b90903a3666bf4a2
blacklyquid <jeremy.w.stansfield@gmail.com>
2022-08-11 15:29:20 -0400

    Update stream_capture.py

--- stream_capture.py
+++ stream_capture.py
@@ -14,7 +14,7 @@ class stream_capture:
 	
 	def get_blob(self):
 		if self.read():
-			#frame = imutils.resize(self.frame, width=400)
+			frame = imutils.resize(self.frame, width=400)
 			blob = cv2.dnn.blobFromImage(cv2.resize(self.frame, (300, 300)), 0.007843, (300, 300), 127.5)
 			return blob
 		else:
